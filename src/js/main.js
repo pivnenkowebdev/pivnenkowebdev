897780aa67635bdfc7b197717c84f9c1a44cd7fd
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-07-17 10:54:39 +0300

    refactor: change design

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-import 'normalize.css';
-import '../style/common/base.scss';
-import '../style/sections/header.scss';
-import '../style/components/burger.scss';
 import '../js/mobile-menu.js';
 import '../js/header-scroll.js';
+import '../style/main.scss';
